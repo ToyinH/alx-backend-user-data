0cc7fa126ec3af738497179759da37cc11a30fda
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-11 16:19:14 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,11 +18,19 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
+
     def __init__(self, fields: List[str]):
+        """
+        init function
+        """
         super().__init__(self.FORMAT)
         self.fields = fields
 
+
     def format(self, record: logging.LogRecord) -> str:
+        """
+        format function
+        """
         record.message = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
         return super().format(record)
 

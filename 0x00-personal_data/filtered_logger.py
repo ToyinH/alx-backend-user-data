1c26c1086b306530f203906d23a96950faeac0dc
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-11 16:17:30 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,25 @@ Regex-ing
 
 import re
 from typing import List
+import logging
+# from filtered_logger import filter_datum
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.message = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -27,3 +46,4 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         message, 
         flags=re.IGNORECASE
     )
+

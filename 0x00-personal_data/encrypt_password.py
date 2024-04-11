3e2006eb95451b0b96dfb9572e19956507f54cdf
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-11 16:42:11 +0100

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -20,3 +20,19 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password.
+
+    Args:
+    hashed_password: A byte string representing the salted, hashed password.
+    password: A string representing the plain-text password.
+
+    Returns:
+    A boolean indicating whether the provided password
+    matches the hashed password.
+    """
+    # Check if the provided password matches the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

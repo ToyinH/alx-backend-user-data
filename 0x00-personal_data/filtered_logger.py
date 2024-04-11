30133181551085c2b21bab266f1757b82d6d0eed
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-11 16:08:32 +0100

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates specified fields in a log message.
+
+    Arguments:
+    fields: A list of strings representing the fields to obfuscate.
+    redaction: A string representing the redaction
+    value to replace the fields with.
+    message: A string representing the log line.
+    separator: A string representing the character separating
+    all fields in the log line.
+
+    Returns:
+    A string with specified fields obfuscated.
+    """
+    return re.sub(r'(?<=^|\b)(' + '|'.join(fields) + r')(?=\b|$)', redaction, message)

bae1f85135426d5f501b7aff2295b779b8c445b1
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-18 06:41:11 +0100

    update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Module handling routes for the API.
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-
 if getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
 
 if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-
 @app.before_request
 def before_request() -> str:
     """
-    before_request
+    Executes before processing a request.
     """
     if auth is None:
         return
@@ -43,34 +39,30 @@ def before_request() -> str:
         if auth.current_user(request) is None:
             abort(403)
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """
-    Not found handler
+    Handles 404 errors.
     """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.errorhandler(401)
 def not_authorized(error) -> str:
     """
-    Not authorized handler
+    Handles 401 errors.
     """
     return jsonify({"error": "Unauthorized"}), 401
-
 
 @app.errorhandler(403)
 def not_allowed(error) -> str:
     """
-    Not allowed handler
+    Handles 403 errors.
     """
     return jsonify({"error": "Forbidden"}), 403
 
-
 if __name__ == "__main__":
     """
-    main function
+    Main function to run the application.
     """
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")

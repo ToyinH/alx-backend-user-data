dbd42447eb075878542366e1ae41867dca8fa8ef
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-26 05:14:25 +0100

    update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,16 +8,21 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     SQLAlchemy model representing the User table.
 
     Attributes:
         id (int): The integer primary key.
-        email (str): A non-nullable string representing the email address of the user.
-        hashed_password (str): A non-nullable string representing the hashed password of the user.
-        session_id (str): A nullable string representing the session ID of the user.
-        reset_token (str): A nullable string representing the reset token of the user.
+        email (str): A non-nullable string representing
+        the email address of the user.
+        hashed_password (str): A non-nullable
+        string representing the hashed password of the user.
+        session_id (str): A nullable string representing the
+        session ID of the user.
+        reset_token (str): A nullable string representing
+        the reset token of the user.
     """
 
     __tablename__ = 'users'

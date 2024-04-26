57120d59f8310c08abbca765c01b442104a24209
Toyin Obafemi <byhisgraceabba@gmail.com>
2024-04-26 05:33:37 +0100

    update for pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,15 +7,17 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def welcome():
     """
     Endpoint to return a welcome message as JSON.
-    
+
     Returns:
         dict: A JSON payload with a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
